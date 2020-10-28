c7225b283a87d62feb343639837ae9836bb8943e
Martin Slaby <martinslaby70@gmail.com>
2020-10-28 23:40:56 +0100

    root query

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import graphqlHTTP from 'graphql';
+import schema from './schema/schema';
 
 const app = express();
 const PORT = 4000;
 
 
 app.use('/graphql', graphqlHTTP({
-
+    schema: schema
 }));
 
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,2 +1,32 @@
-import graphql from 'graphql';
+import graphql, { GraphQLSchema } from 'graphql';
 
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    field: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        gendre: {type: GraphQLString}
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {
+                id: {type: GraphQLString}
+            },
+            resolve(parent, args){
+               //code to get data from database 
+            }
+        }
+    }
+});
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
