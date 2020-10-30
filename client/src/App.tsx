33e641bf31e90f1ac64424ecd6cbaf352eedb293
Martin Slabý <martin.slaby29@seznam.cz>
2020-10-30 23:11:46 +0100

    added client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import BookList from './components/bookList';
+
+const App = () => {
+
+
+  return(
+    <div>
+      <BookList />
+    </div>
+  )
+}
+
+export default App;
---
+++ client/src/components/bookList.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+ 
+const BookList = () => {
+
+
+
+    return(
+        <div>
+            <ul id="book-list">
+                <li>Bookname</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BookList;
