a8baeafb8d33a24911efe5243476039692b5aaa7
Martin Slabý <martin.slaby29@seznam.cz>
2020-10-30 23:40:39 +0100

    apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+// components
 import BookList from './components/bookList';
+
+//apollo setup
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache(),
+});
+
 
 const App = () => {
 
 
   return(
     <div>
-      <BookList />
+      <ApolloProvider client={client}>
+        
+        <BookList />
+        
+      </ApolloProvider>
     </div>
   )
 }
